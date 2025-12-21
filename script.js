49443038509d556a8becc7ab5e791f7db2075e11
HimanshuSharma-07 <sharmahimanshu82194@gmail.com>
2025-12-21 23:10:44 +0530

    add functionality to all objects and reset button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const filters = {
+let filters = {
     brightness: {
         value: 100,
         min: 0,
@@ -6,12 +6,6 @@ const filters = {
         unit: "%"
     },
     contrast: {
-        value: 100,
-        min: 0,
-        max: 200,
-        unit: "%"
-    },
-    exposure: {
         value: 100,
         min: 0,
         max: 200,
@@ -63,7 +57,10 @@ const filters = {
 
 const imageCanvas = document.querySelector("#image-canvas")
 const imageInput = document.querySelector("#image-input")
+const resetFilters = document.querySelector("#reset-btn")
 const canvasCtx = imageCanvas.getContext("2d")
+let file = null
+let image = null
 
 const filtersContainer = document.querySelector(".filters")
 
@@ -84,35 +81,133 @@ function createFilterElement(name, unit = "%", value, min , max){
      div.append(p)
      div.append(input)
 
+     input.addEventListener('input', (e) => {
+        filters[ name ].value = input.value
+        applyFilters()
+          
+        
+     })
+
     return div
 }
 
-Object.keys(filters).forEach( (key) =>{
-    
+function createFilter(){
 
-    const filterElement = createFilterElement(key, filters[key].unit, filters[key].value, filters[key].min, filters[key].max)
-    filtersContainer.appendChild(filterElement)
-      
-})
+    Object.keys(filters).forEach( (key) =>{
+        
+        const filterElement = createFilterElement(key, filters[key].unit, filters[key].value, filters[key].min, filters[key].max)
+        filtersContainer.appendChild(filterElement)
+        
+    })
+}
+
+createFilter()
 
 imageInput.addEventListener("change", (e) => {
-    const file  = e.target.files[0];
+    file  = e.target.files[0];
     const imagePlaceholder = document.querySelector(".placeholder")
+    imageCanvas.style.display = "block"
     imagePlaceholder.style.display = "none"
     
     const img = new Image()
     img.src = URL.createObjectURL(file)
     
     img.onload = () =>{
+        image = img
         imageCanvas.height = img.height
         imageCanvas.width = img.width
         canvasCtx.drawImage(img, 0, 0)
+        
+        
     }
     
-    
-
-    
 })
+
+function applyFilters(){
+    canvasCtx.clearRect(0, 0, imageCanvas.width, imageCanvas.height)
+    canvasCtx.filter = `
+    brightness(${filters.brightness.value}${filters.brightness.unit})
+    contrast(${filters.contrast.value}${filters.contrast.unit})
+    saturate(${filters.saturation.value}${filters.saturation.unit})
+    hue-rotate(${filters.hueRotation.value}${filters.hueRotation.unit}) 
+    blur(${filters.blur.value}${filters.blur.unit})
+    grayscale(${filters.grayScale.value}${filters.grayScale.unit}) 
+    sepia(${filters.sepia.value}${filters.sepia.unit})
+    opacity(${filters.opacity.value}${filters.opacity.unit}) 
+    invert(${filters.invert.value}${filters.invert.unit})
+    `.trim()
+
+    canvasCtx.drawImage(image, 0, 0)
+    
+}
+
+
+resetFilters.addEventListener("click", () => {
+    filters = {
+    brightness: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%"
+    },
+    contrast: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%"
+    },
+    saturation: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%"
+    },
+    hueRotation: {
+        value: 0,
+        min: 0,
+        max: 360,
+        unit: "deg"
+    },
+    blur: {
+        value: 0,
+        min: 0,
+        max: 20,
+        unit: "px"
+    },
+    grayScale: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%"
+    },
+    sepia: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%"
+    },
+    opacity: {
+        value: 100,
+        min: 0,
+        max: 100,
+        unit: "%"
+    },
+    invert: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%"
+    },
+}
+
+applyFilters()
+filtersContainer.innerHTML = ""
+createFilter()
+
+
+})
+
+
 
 
 
