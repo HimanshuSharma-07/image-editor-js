6b0952f2be04ee9a11672e10fd35c7e5e651756f
HimanshuSharma-07 <sharmahimanshu82194@gmail.com>
2025-12-25 22:16:51 +0530

    complete projects

--- script.js
+++ script.js
@@ -58,7 +58,9 @@ let filters = {
 const imageCanvas = document.querySelector("#image-canvas")
 const imageInput = document.querySelector("#image-input")
 const resetFilters = document.querySelector("#reset-btn")
+const downloadButton = document.querySelector("#download-btn")
 const canvasCtx = imageCanvas.getContext("2d")
+const presetsContainer = document.querySelector(".presets")
 let file = null
 let image = null
 
@@ -205,6 +207,169 @@ filtersContainer.innerHTML = ""
 createFilter()
 
 
+})
+
+downloadButton.addEventListener('click', () => {
+    const link = document.createElement("a")
+    link.download = "edited-image.png"
+    link.href = imageCanvas.toDataURL()
+    link.click()
+
+})
+
+const presets = {
+    normal: {
+        brightness: 100,
+        contrast: 100,
+        saturation: 100,
+        hueRotation: 0,
+        blur: 0,
+        grayScale: 0,
+        sepia: 0,
+        opacity: 100,
+        invert: 0,
+    },
+
+    drama: {
+        brightness: 105,
+        contrast: 150,
+        saturation: 140,
+        hueRotation: 0,
+        blur: 0,
+        grayScale: 0,
+        sepia: 10,
+        opacity: 100,
+        invert: 0,
+    },
+
+    vintage: {
+        brightness: 110,
+        contrast: 90,
+        saturation: 80,
+        hueRotation: 10,
+        blur: 1,
+        grayScale: 10,
+        sepia: 40,
+        opacity: 100,
+        invert: 0,
+    },
+
+    oldSchool: {
+        brightness: 100,
+        contrast: 120,
+        saturation: 60,
+        hueRotation: 0,
+        blur: 0,
+        grayScale: 20,
+        sepia: 70,
+        opacity: 100,
+        invert: 0,
+    },
+
+    cinematic: {
+        brightness: 95,
+        contrast: 140,
+        saturation: 85,
+        hueRotation: 5,
+        blur: 0,
+        grayScale: 5,
+        sepia: 10,
+        opacity: 100,
+        invert: 0,
+    },
+
+    warm: {
+        brightness: 108,
+        contrast: 105,
+        saturation: 120,
+        hueRotation: 8,
+        blur: 0,
+        grayScale: 0,
+        sepia: 15,
+        opacity: 100,
+        invert: 0,
+    },
+
+    cool: {
+        brightness: 102,
+        contrast: 110,
+        saturation: 90,
+        hueRotation: 190,
+        blur: 0,
+        grayScale: 0,
+        sepia: 0,
+        opacity: 100,
+        invert: 0,
+    },
+
+    blackAndWhite: {
+        brightness: 100,
+        contrast: 130,
+        saturation: 0,
+        hueRotation: 0,
+        blur: 0,
+        grayScale: 100,
+        sepia: 0,
+        opacity: 100,
+        invert: 0,
+    },
+
+    highContrast: {
+        brightness: 100,
+        contrast: 180,
+        saturation: 130,
+        hueRotation: 0,
+        blur: 0,
+        grayScale: 0,
+        sepia: 0,
+        opacity: 100,
+        invert: 0,
+    },
+
+    softFade: {
+        brightness: 115,
+        contrast: 80,
+        saturation: 70,
+        hueRotation: 0,
+        blur: 1,
+        grayScale: 0,
+        sepia: 10,
+        opacity: 95,
+        invert: 0,
+    },
+
+    inverted: {
+        brightness: 100,
+        contrast: 100,
+        saturation: 100,
+        hueRotation: 0,
+        blur: 0,
+        grayScale: 0,
+        sepia: 0,
+        opacity: 100,
+        invert: 100,
+    }
+};
+
+
+Object.keys(presets).forEach( (presetName) =>{
+    const presetButton = document.createElement("button")
+    presetButton.classList.add("btn")
+    presetButton.innerText = presetName
+    presetsContainer.appendChild(presetButton) 
+    
+     presetButton.addEventListener('click', () => {
+        const preset = presets[presetName]
+        
+        Object.keys(preset).forEach( (filterName) => {
+            filters[ filterName ].value = preset[ filterName]
+        })
+
+        applyFilters()
+        filtersContainer.innerHTML = ""
+        createFilter()
+        
+     })
 })
 
 
